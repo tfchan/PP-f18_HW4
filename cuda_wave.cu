8457bb67cf83fb1e1d601e05390fbb4d67ca6284
tfchan <cseelemon@gmail.com>
2018-11-30 20:45:29 +0800

    Add argument control to cuda_wave.cu
    Add time steps and total points arguments control to main program of
    cuda_wave.cu

---
+++ cuda_wave.cu
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int nsteps,     // Number of time steps
+    tpoints;    // Total points along string
+
+int main(int argc, char *argv[]) {
+    sscanf(argv[1],"%d",&tpoints);
+    sscanf(argv[2],"%d",&nsteps);
+
+    return 0;
+}
