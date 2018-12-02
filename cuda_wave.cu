f1d7a1c041b8303dd922ca72f8dd80a9d52a2070
tfchan <cseelemon@gmail.com>
2018-12-02 18:34:49 +0800

    Add kernel function structure and invoke it
    Add kernel funtion structure and call it in main with specific grid
    size and block size

--- cuda_wave.cu
+++ cuda_wave.cu
@@ -5,6 +5,8 @@
 #define MAXSTEPS 1000000
 #define MINPOINTS 20
 
+const int kThreadsPerBlock = 256;
+
 int nsteps,     // Number of time steps
     tpoints;    // Total points along string
 float values[MAXPOINTS + 2];    // Values at time t
@@ -29,6 +31,10 @@ void check_param(void) {
     }
 }
 
+// Kernel for computing value of a point at specific time with speific time step
+__global__ void init_and_update(float *dValues, int tpoints, int nsteps) {
+}
+
 void printfinal() {
     for (int i = 1; i <= tpoints; i++) {
         printf("%6.4f ", values[i]);
@@ -40,12 +46,14 @@ void printfinal() {
 int main(int argc, char *argv[]) {
     float *dValues; // Values in device
     int size = (MAXPOINTS + 2) * sizeof(float); // Size of memory to store values
+    int numOfBlocks;    // Number of blocks used to call kernel
 
     sscanf(argv[1],"%d",&tpoints);
     sscanf(argv[2],"%d",&nsteps);
     check_param();
     cudaMalloc(&dValues, size);
-
+    numOfBlocks = (tpoints - 1) / kThreadsPerBlock + 1; // Compute and ceil number of block
+    init_and_update<<<numOfBlocks, kThreadsPerBlock>>>(dValues, tpoints, nsteps);
     cudaMemcpy(values, dValues, size, cudaMemcpyDeviceToHost);
     printf("Printing final results...\n");
     printfinal();
