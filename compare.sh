98b868db5c064f5bc4321ef8592f2b1000f3acd7
tfchan <cseelemon@gmail.com>
2018-12-03 17:12:41 +0800

    Add a script to compare serial and CUDA program
    Add a script to compare execution time, final result between serial version and CUDA version

---
+++ compare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+usage="Usage: $0 <total points> <time step>"
+serial_result_file="a"
+cuda_result_file="b"
+diff_result_file="diff_result"
+if [ $# -eq 2 ]; then
+    make all
+    echo "Serial version:"
+    time ./serial_wave $1 $2 > $serial_result_file
+    echo "CUDA version:"
+    time ./cuda_wave $1 $2 > $cuda_result_file
+    nDiff=$(diff -y --suppress-common-lines $serial_result_file $cuda_result_file | wc -l)
+    echo "Numeber of different line: ${nDiff}"
+    echo "Diff result store at ${diff_result_file}"
+    diff $serial_result_file $cuda_result_file > $diff_result_file
+    rm $serial_result_file $cuda_result_file
+else
+    echo $usage
+    exit 1
+fi
